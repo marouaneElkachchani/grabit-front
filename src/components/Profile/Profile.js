d817e8f5eafb345faf4e77a02bca9e73f6617850
Marouane El Kachchani <marouaneelkachchani@Gemographys-MacBook-Pro.local>
2020-12-30 15:41:03 +0100

    Add HTML/CSS for Profile screen

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,49 +3,82 @@ import './Profile.css';
 
 class Profile extends React.Component {
     render() {
-        return (
-            <div className="profile">
-                <div className="top">
-                    <a href="">
-                        <img src="./assets/grabit-icon.png" alt="Grabit" />
-                    </a>
-                    <button>Request an Order</button>
-                    <a href="">
-                        <p>Patek Philippe</p>
-                    </a>
-                </div>
-                <div className="main">
-                    <div className="main-left">
-                            <div className="main-left-top">
-                                <p>Patek Philippe</p>
-                            </div>
-                            <ul>
-                                <a href="">        
-                                    <li>
-                                        <p>Profile Settings</p>
-                                    </li>
-                                </a>    
-                                <a href="">
-                                    <li>
-                                        <p>Requests</p>
-                                    </li>
-                                </a>
-                                <a href="">
-                                    <li>
-                                        <p>Address</p>
-                                    </li>
-                                </a>
-                            </ul>
+            return (
+                <div className="profile">
+                    <div className="top">
+                        <a href="">
+                            <img src="./assets/grabit-icon.png" alt="Grabit" />
+                        </a>
+                        <button>Request an Order</button>
+                        <a href="">
+                            <p>Patek Philippe</p>
+                        </a>
                     </div>
-                    <div className="main-right">
-                        <div className="main-right-top">
-                            <h3>Profile Settings</h3>
+                    <div className="main">
+                        <div className="main-left">
+                                <div className="main-left-top">
+                                    <p>Patek Philippe</p>
+                                </div>
+                                <ul>
+                                    <a href="">        
+                                        <li>
+                                            <img id="user-dark" src="./assets/user-dark.png" alt="Grabit" />
+                                            <img id="user-white" src="./assets/user-white.png" alt="Grabit" />
+                                            <p>Profile Settings</p>
+                                        </li>
+                                    </a>    
+                                    <a href="">
+                                        <li>
+                                            <img id="requests-dark" src="./assets/requests-dark.png" alt="Grabit" />
+                                            <img id="requests-white" src="./assets/requests-white.png" alt="Grabit" />
+                                            <p>Requests</p>
+                                        </li>
+                                    </a>
+                                    <a href="">
+                                        <li>
+                                            <img id="address-dark" src="./assets/address-dark.png" alt="Grabit" />
+                                            <img id="address-white" src="./assets/address-white.png" alt="Grabit" />
+                                            <p>Address</p>
+                                        </li>
+                                    </a>
+                                </ul>
+                        </div>
+                        <div className="main-right" >
+                            <div className="main-right-top">
+                                <h3>Profile Settings</h3>
+                            </div>
+                            <div className="main-right-form">
+                                <div className="main-right-form-inputs">
+                                    <section class="input">
+                                        <label for="name">First & Last Name</label>
+                                        <br/>
+                                        <input type="text" name="name" id="name"/>
+                                    </section>
+                                    <section class="input">
+                                        <label for="email">Email</label>
+                                        <br/>
+                                        <input type="email" name="email" id="email"/>
+                                    </section>
+                                    <section class="input">
+                                        <label for="phone">Phone</label>
+                                        <br/>
+                                        <input type="text" name="phone" id="phone"/>
+                                    </section>
+                                    <br/>
+                                    <button>Update</button>
+                                </div>
+                                <div className="main-right-form-image">
+                                    <img src="./assets/oval.png" alt="Grabit" />
+                                    <br/>
+                                    <button id="upload" >Upload</button>
+                                    <button id="remove" >Remove</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            );
-        }
+                );
     }
-    
+}
+
 export default Profile;
